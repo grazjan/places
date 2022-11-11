4b442cc8245b0888f903c50a127359e82b47c233
Gracjan <g.drozdek@ixtensa.de>
2022-11-11 23:29:30 +0100

    init react router & basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </Router>
   )
 }
 
