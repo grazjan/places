783c8b8eb2e8b602d58632e0917ad5b1b1288718
Gracjan <g.drozdek@ixtensa.de>
2022-11-12 18:58:55 +0100

    set custom button global styles

--- client/src/Theme.js
+++ client/src/Theme.js
@@ -1,5 +1,18 @@
 import { createTheme } from '@mui/material'
 
+/* Border radius */
+const borderRadius = {
+    small: "10px",
+    medium: "25px",
+    large: "30px"
+}
+
+/* Buttons */
+const button = {
+    padding: "8px 24px",
+    hoverColor: '#2d3436'
+}
+
 export const theme = createTheme({
     palette: {
         primary: {
@@ -16,5 +29,28 @@ export const theme = createTheme({
     typography: {
         fontFamily: "'Source Sans Pro', 'Helvetica', 'Arial', sans-serif", 
         fontSize: 16
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                contained: {
+                    borderRadius: borderRadius.medium,
+                    padding: button.padding,
+                    border: "none",
+                    '&:hover': {
+                        backgroundColor: button.hoverColor
+                    }
+                },
+                outlined: {
+                    borderRadius: borderRadius.medium,
+                    padding: button.padding,
+                    '&:hover': {
+                        backgroundColor: button.hoverColor,
+                        borderColor: button.hoverColor,
+                        color: '#fff',
+                    }
+                }
+            }
+        }
     }
 });
