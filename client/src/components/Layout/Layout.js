f5ec07a09c0fd1aebbb45f592ecd19009fcec126
Gracjan <g.drozdek@ixtensa.de>
2022-11-12 23:59:53 +0100

    several changes, added navbar and width & height properties

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,20 +1,29 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Menu from '../Menu/Menu'
 
 const Layout = ({ children }) => {
-  return (
-    <>
-    <Box component="header">
 
-    </Box>
-    <Box component="main">
-        {children}
-    </Box>
-    <Box component="footer">
-        
-    </Box>
-    </>
-  )
+    const headerHeight = 100;
+    const siderbarWidth = 120;
+
+    return (
+        <>
+        <Box component="header">
+            <Navbar height={headerHeight} />
+        </Box>
+        <Box component="nav">
+            <Menu headerHeight={headerHeight} sidebarWidth={siderbarWidth} />
+        </Box>
+        <Box component="main" sx={{ mt: headerHeight+"px", ml: { xs: 0, sm: siderbarWidth+"px" } }}>
+            {children}
+        </Box>
+        <Box component="footer">
+            
+        </Box>
+        </>
+    )
 }
 
 export default Layout
