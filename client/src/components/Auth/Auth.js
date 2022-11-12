3330188c5379ea7fc2978cc852c3aa82884e5839
Gracjan <g.drozdek@ixtensa.de>
2022-11-12 18:59:30 +0100

    create auth sing in and sing up form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,8 +1,92 @@
-import React from 'react'
+import styled from '@emotion/styled'
+import { Box, Button, Link, TextField, Typography } from '@mui/material'
+import Google from '@mui/icons-material/Google';
+import { useState } from 'react'
+
+/* styled */
+const StyledForm = styled('form')(({ theme }) => ({
+  padding: theme.spacing(7),
+  width: "85%",
+  '& .MuiTextField-root': {
+    margin: `${theme.spacing(1)} 0`
+  },
+  '& .MuiLink-root': {
+    cursor: "pointer",
+  },
+  '& .MuiButtonBase-root': {
+    marginTop: `${theme.spacing(2)}`,
+    width: "100%"
+  }
+}))
 
 const Auth = () => {
+
+  const [formData, setFormData] = useState({ username: '', mail: '', password: '', confirmpw: '' })
+  const [isSigningIn, setSigningIn] = useState(true)
+
+  const handleForm = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
   return (
-    <div>Auth</div>
+    <StyledForm
+      component="form"
+      noValidate
+    >
+
+      {/* Sign in form */}
+      <TextField 
+        required
+        name="username"
+        value={formData.username}
+        label={isSigningIn ? "Email or Username" : "Username"}
+        onChange={handleForm}
+        fullWidth
+      />
+      {!isSigningIn && 
+        <TextField 
+          required
+          name="mail"
+          type="email"
+          value={formData.mail}
+          label="Email"
+          onChange={handleForm}
+          fullWidth
+        />
+      }
+      <TextField 
+        required
+        type="password"
+        name="password"
+        value={formData.passowrd}
+        label="Password"
+        onChange={handleForm}
+        fullWidth
+      />
+      {!isSigningIn && 
+        <TextField 
+          required
+          type="password"
+          name="confirmpw"
+          value={formData.confirmpw}
+          label="Confirm Password"
+          onChange={handleForm}
+          fullWidth
+        />
+      }
+      <Box>
+        <Typography variant="body2">
+          {isSigningIn ? 'Dont have an Account?' : 'Already have an Account?'}
+          <Link onClick={() => setSigningIn(!isSigningIn)}>{isSigningIn ? 'Sign up' : 'Sign in'}</Link>
+        </Typography>
+      </Box>
+      <Button variant="contained">
+        {isSigningIn ? 'Sign up' : 'Sign in'}
+      </Button>
+      {/* <Button variant="outlined" startIcon={<Google />}>
+        Google
+      </Button> */}
+    </StyledForm>
   )
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,18 +2,22 @@ import { Box, Grid } from '@mui/material'
 import Image from 'mui-image'
 import Auth from '../components/Auth/Auth';
 import LoginBg from '../images/bg-login.jpg';
+import Logo from '../images/logo-no-background.png';
 
 const Login = () => {
   return (
     <Box>
         <Grid container sx={{ height: "100vh" }}>
             <Grid item md={5}>
-                <Auth/>
+                <Box pt={15} sx={{ display: "flex", flexDirection: "column" , justifyContent: "center", alignItems: "center" }}>
+                    <Image src={Logo} alt="Places logo" width="240px" height="auto" />
+                    <Auth/>
+                </Box>
             </Grid>
-            <Grid item md={7}>
+            <Grid item md={7} sx={{ display: { xs: 'none', md: 'block' } }}>
                 <Box sx={{ position: "relative", height: "100%" }}>
                     <Image src={LoginBg} alt="Login Page" />
-                    <Box sx={{ position: "absolute", top: "0", left: "0", right: "0", bottom: "0", background: "rgba(0, 0, 0, 0.3)" }}/>
+                    <Box sx={{ position: "absolute", top: "0", left: "0", right: "0", bottom: "0", background: "rgba(0, 0, 0, 0.4)" }}/>
                 </Box>
             </Grid>
         </Grid>
