f841941ec55ab5f470f6d7799e1936392f1d455d
Gracjan <g.drozdek@ixtensa.de>
2022-11-13 01:18:44 +0100

    init server, installed packages, connection to mongodb

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-// index.js 
+import express from "express";
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import mongoose from "mongoose";
+
+//Dotenv
+dotenv.config();
+
+//init
+const app = express();
+app.use(cors());
+
+
+
+// MongoDB connection
+const CONNECTION_URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.9tohf.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`)))
+    .catch((err) => console.log(err.message))
