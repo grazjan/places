7bfdfa08c5c2c462bebdc7ed85ee2e1997430547
Gracjan <g.drozdek@ixtensa.de>
2023-01-07 21:55:52 +0100

    extended mongoose schema

--- server/models/place.js
+++ server/models/place.js
@@ -6,6 +6,7 @@ const placeSchema = mongoose.Schema({
     images: [String],
     lat: Number,
     long: Number,
+    city: String,
     countryCode: String,
     location: String,
     tags: [String],
