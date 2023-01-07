0e5f2c801ddfa1dc135221297419d55fd651fb8b
Gracjan <g.drozdek@ixtensa.de>
2023-01-07 21:57:18 +0100

    redux endpoints for patch, post, delete and upload files

--- client/src/features/apiSlice.js
+++ client/src/features/apiSlice.js
@@ -3,11 +3,57 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: 'places',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/' }),
+    tagTypes: ['Places'],
     endpoints: builder => ({
         getPlaces: builder.query({
-            query: () => '/places'
+            query: () => '/places',
+            transformResponse: res => res.sort((a, b) => b.id - a.id), // Sort in reverse order
+            providesTags: ['Places']
+        }),
+        getPlace: builder.query({
+            query: (id) => `/places/${id}`,
+            providesTags: ['Places']
+        }),
+        addPlace: builder.mutation({
+            query: (place) => ({
+                url: '/places',
+                method: 'POST',
+                body: place
+            }),
+            invalidatesTags: ['Places'],
+        }),
+        updatePlace: builder.mutation({
+            query: (place) => ({
+                url: `/places/${place.id}`,
+                method: 'PATCH',
+                place
+            }),
+            invalidatesTags: ['Places'],
+        }),
+        deletePlace: builder.mutation({
+            query: ({ id }) => ({
+                url: `/places/${id}`,
+                method: 'DELETE',
+                body: id
+            }),
+            invalidatesTags: ['Places'],
+        }),
+        uploadFile: builder.mutation({
+            query: (file) => ({
+                url: '/upload',
+                method: 'POST',
+                body: file
+            })
         })
     })
 });
 
-export const { useGetPlacesQuery } = apiSlice;
+export const { 
+    useGetPlacesQuery, 
+    useLazyGetPlacesQuery,
+    useLazyGetPlaceQuery,
+    useAddPlaceMutation,
+    useUpdatePlaceMutation,
+    useDeletePlaceMutation,
+    useUploadFileMutation
+} = apiSlice;
